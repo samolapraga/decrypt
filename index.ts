485271e9aa57a4cd58e9cc69453f6a163743db19
samolapraga <samoila.alexandru@gmail.com>
2020-05-11 10:52:39 +0300

    2 files modified

--- index.ts
+++ index.ts
@@ -1,31 +1,18 @@
 import * as CryptoJS from "crypto-js";
 import * as aesJS from "aes-js";
+import * as browserify from "browserify";
 let text = 'eCMl38E/KLGSJOphNEDajA==,yKqhhRW1q6yGD3l4q54veb04gSvv7pWdLbNdIctAFtlwNMMi13iL5mhiGkNMh/xxHaZhUBUqIUcDSG473eW/MdA+OzfpFND1cd7URx314VAYAALXw5ItL52ZPz6DfwaSSkvBYQ0oj7KERoFTgyUsrLIXluNtjgKXKXbT5CVDPqQKmakq+eSoGbysuvAOrK+Uc9RJ/3aWg66hWcuWtuAAoa4Z3viiwsapQdZAygA3eKQkxVIDaPf5EUu6N3k1mbrPXQn+skRBZ+y8NNhx3tXGwFP+6wYOxbo0V4cV1hctsoCWVu8jzJ85mX8+wNsf5MYTEvMaLJm8W6gdD08rydZih2gCGW2JNxLwaz9u6wPaKsFu0hOrNKs7Q1I5PDCpBFHEP061F17Jn0s1CbclOm18z6duHFYXzMT1oPVyl598loZUFwkIfrLEcuv6xFaLG2U5lCC8Zd2IsaolULM/ZFxIl8sA3+t6pmoUmIZq0p2tJa6A8BkDs1pK+O8NZ2U2qOEoTOmnRuJVEzli4g5yLL43vA==';
 let Crypto = CryptoJS.Crypto
 function aesDecrypt(data) {
     let key = 'f23b1223092781fca6dc696ad1d9476b';    //length 32
     let arr = data.split(',');
-    let iv = aesJS.utils.Base64.toBytes(arr[0]); 
-    let jayson = CryptoJS.enc.Base64.parse(arr[1]);
-    let k = CryptoJS.enc.Utf8.parse(key);
-    //console.log(iv);
-    //console.log(jayson);
-    //let cipher = CryptoJS.AES.decrypt(jayson, k, {
-    //    iv: iv,
-    //    keySize: 32,
-    //    mode: CryptoJS.mode.CBC,
-    //    format: CryptoJS.format.openssl
-    //    //padding: CryptoJS.pad.NoPadding
-    //});
-    //console.log(cipher.toString(CryptoJS.enc.Utf16));
-    var encryptedBytes = aesJS.utils.hex.toBytes(jayson);
-    var aesCbc = new aesJS.ModeOfOperation.cbc(key, iv);
-    var decryptedBytes = aesCbc.decrypt(jayson);
- 
-    // Convert our bytes back into text
-    var decryptedText = aesJS.utils.utf8.fromBytes(decryptedBytes);
-    return decryptedText.toString(CryptoJS.enc.Utf8);
-    //return cipher.toString(CryptoJS.enc.Utf8);
+    let iv = Buffer.from(arr[0], 'base64');//CryptoJS.enc.Base64.parse(arr[0]);//atob(arr[0]); 
+    let jayson = Buffer.from(arr[1], 'base64');//atob(CryptoJS.enc.Base64.pars
+
+    var crypt = require('crypto');
+    var decipher = crypt.createDecipheriv("aes-256-cbc", key, iv);
+    var decryptedIpnResponse = Buffer.concat([decipher.update(jayson), decipher.final()]).toString();
+    return JSON.parse(decryptedIpnResponse);
 }
 
 console.log(aesDecrypt(text));
